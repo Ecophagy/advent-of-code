608284dedb7d1d709e0158e8b6876eae23c6e97f
Oliver Hill <oliver.hill@q-bot.co>
2024-12-02 11:26:07 +0000

    Refactor part 1 solution for code reuse

--- Day1_1.py
+++ Day1_1.py
@@ -1,6 +1,5 @@
 
-
-if __name__ == "__main__":
+def read_list():
     left_list = []
     right_list = []
     with open("input/Day1.txt") as f:
@@ -10,6 +9,10 @@ if __name__ == "__main__":
             left_list.append(int(left))
             right_list.append(int(right))
 
+    return left_list, right_list
+
+# Part 1 Solution
+def list_difference(left_list, right_list):
     left_list.sort()
     right_list.sort()
 
@@ -19,3 +22,8 @@ if __name__ == "__main__":
         total_difference += difference
 
     print(total_difference)
+
+
+if __name__ == "__main__":
+    left_list, right_list = read_list()
+    list_difference(left_list, right_list)
