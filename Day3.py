025679505eb00dbc40eebf4e0aeaec141ed63561
Ecophagy <olly.spoon@gmail.com>
2024-12-03 10:23:18 +0000

    Day 3 part 2 solution

--- Day3.py
+++ Day3.py
@@ -4,7 +4,7 @@ def read_data():
     with open("input/Day3.txt") as f:
         return f.read()
 
-def find_multiply_commands(data):
+def find_multiply_commands_naive(data):
     multiply_commands = []
     regex = r"mul\((\d{1,3}),(\d{1,3})\)"
     matches = re.findall(regex, data)
@@ -12,10 +12,18 @@ def find_multiply_commands(data):
         multiply_commands.append(match)
     return multiply_commands
 
+def find_enabled_multiplies(data):
+    multiply_commands = []
+    regex = r"(^|do\(\))(.*?)(don't\(\)|$)" # Requires input to be on one line
+    matches = re.findall(regex, data)
+    for match in matches:
+        multiply_commands += (find_multiply_commands_naive(match[1]))
+    return multiply_commands
+
 if __name__ == "__main__":
     data = read_data()
     multiplies = []
-    commands = find_multiply_commands(data)
+    commands = find_enabled_multiplies(data)
     total = 0
     for command in commands:
         total = total + int(command[0]) * int(command[1])
