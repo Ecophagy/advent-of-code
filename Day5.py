5a631fce735e9a9679eff40a94fd11f212efeda1
Ecophagy <olly.spoon@gmail.com>
2024-12-05 14:19:30 +0000

    Day 5 part 2 solution

--- Day5.py
+++ Day5.py
@@ -40,13 +40,34 @@ def calculate_middle_page_sum(valid_updates):
         middle_page_sum += update[middle_index]
     return middle_page_sum
 
+def fix_update(rules, update):
+    while not validate_update(rules, update):
+        index = 0
+        for page in update:
+            if page in rules:
+                for i in range(0, index):
+                    if update[i] in rules[page]:
+                        # Move item one spot earlier in list
+                        update.insert(index - 1, update.pop(index))
+            index += 1
+    return update
+
 
 if __name__ == "__main__":
     rules = parse_rules()
     updates = parse_updates()
     valid_updates = []
+    invalid_updates = []
     for update in updates:
         if validate_update(rules, update):
             valid_updates.append(update)
+        else:
+            invalid_updates.append(update)
+
     middle_page_sum = calculate_middle_page_sum(valid_updates)
-    print(middle_page_sum)
+    print(f"Middle sum of valid updates: {middle_page_sum}")
+
+    fixed_updates = []
+    for update in invalid_updates:
+        fixed_updates.append(fix_update(rules, update))
+    print(f"Middle sum of fixed invalid updates: {calculate_middle_page_sum(fixed_updates)}")
