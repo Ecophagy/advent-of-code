89fa76d987975feb648df393da68a12ea901442b
Ecophagy <olly.spoon@gmail.com>
2024-12-09 20:19:46 +0000

    Day 9 part 2 solution
    Previous commit was close, but needed to be at higher indentation levels

--- Day9.py
+++ Day9.py
@@ -1,6 +1,6 @@
 
 def read_input():
-    with open("input/Day9_example.txt") as f:
+    with open("input/Day9.txt") as f:
         return f.read()
 
 def uncompress(disk_map):
@@ -34,9 +34,11 @@ def frag(disk):
 
 # Part 2
 def defrag(disk):
+    # Iterate backwards
     j = len(disk) - 1
     while j >= 0:
         if disk[j] != ".":
+            # We found a file!
             file_index = j
             while disk[file_index] == disk[j]:
                 file_index -= 1
@@ -45,6 +47,9 @@ def defrag(disk):
 
             # Find gap from front
             for i in range(len(disk)):
+                if i > j:
+                    # Stop searching if we would defragment the file rightwards
+                    break
                 if disk[i] == ".":
                     gap_index = i
                     while gap_index < len(disk) and disk[gap_index] == ".":
@@ -54,14 +59,17 @@ def defrag(disk):
 
                     # Is the gap big enough?
                     if gap_size >= file_size:
+                        # Yes, defragment the file
                         fill_index = i
                         for k in range(file_index + 1, j + 1):
                             disk[fill_index] = disk[k]
                             disk[k] = "."
                             fill_index += 1
-                            j -= file_size - 1
                         break
-            j -= 1
+            # We found a file, so skip back past it.
+            # Without this, we would move 3 members of a size 4 file in to a size 3 gap!
+            j -= file_size - 1 # -1 because we always move at least 1 step (below)
+        j -= 1
     return disk
 
 
@@ -76,7 +84,7 @@ def calculate_checksum(disk):
 if __name__ == "__main__":
     disk_map = read_input()
     uncompressed_disk = uncompress(disk_map)
-    #fragged_disk = frag(uncompressed_disk)
-    defragged_disk = defrag(uncompressed_disk)
-    print(defragged_disk)
+    #fragged_disk = frag(uncompressed_disk) # Part 1
+    defragged_disk = defrag(uncompressed_disk) # Part 2
+
     print(f"Checksum: {calculate_checksum(defragged_disk)}")
