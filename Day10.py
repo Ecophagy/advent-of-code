4c3720837a820976d6ff282fd8857a7dc1330aef
Ecophagy <olly.spoon@gmail.com>
2024-12-10 20:33:26 +0000

    Day 10 Part 2

--- Day10.py
+++ Day10.py
@@ -23,37 +23,62 @@ def find_trails(grid):
     for row in range(len(grid)):
         for column in range(len(grid[0])):
             if grid[row][column] == 0:
-                counter += len(step(grid, row, column, 0, 9))
+                counter += len(step_part_2(grid, row, column, 0, 9))
     return counter
 
 
-def step(grid, row, column, height, limit):
+# Part 1: Find each head that a trailhead can reach by recursively walking paths until we reach the limit.
+# We need to track which heads we have reached from a trailhead because we ONLY count how many peaks we can reach
+# Not how many different routes there are to get there
+def step_part_1(grid, row, column, height, limit):
     peaks = []
     if height == limit:
         peaks.append((row, column))
         return peaks
     else:
         if row < len(grid) - 1 and grid[row + 1][column] == height + 1:
-            out = step(grid, row + 1, column, height + 1, limit)
-            for peak_location in out:
+            for peak_location in step_part_1(grid, row + 1, column, height + 1, limit):
                 if peak_location not in peaks:
                     peaks.append(peak_location)
         if row > 0 and grid[row - 1][column] == height + 1:
-            for peak_location in step(grid, row - 1, column, height + 1, limit):
+            for peak_location in step_part_1(grid, row - 1, column, height + 1, limit):
                 if peak_location not in peaks:
                     peaks.append(peak_location)
         if column < len(grid[0]) - 1 and grid[row][column + 1] == height + 1:
-            for peak_location in step(grid, row, column + 1, height + 1, limit):
+            for peak_location in step_part_1(grid, row, column + 1, height + 1, limit):
                 if peak_location not in peaks:
                     peaks.append(peak_location)
         if column > 0 and grid[row][column - 1] == height + 1:
-            for peak_location in step(grid, row, column - 1, height + 1, limit):
+            for peak_location in step_part_1(grid, row, column - 1, height + 1, limit):
                 if peak_location not in peaks:
                     peaks.append(peak_location)
         return peaks
 
+# Part 2: This time we DO care about the different routes, so just remove the "if already visited" check
+# Could be simplified by having an int counter instead of an array, but this keeps the API the same.
+# This was the original algorithm - solving part 2 first is a new experience!
+def step_part_2(grid, row, column, height, limit):
+    peaks = []
+    if height == limit:
+        peaks.append((row, column))
+        return peaks
+    else:
+        if row < len(grid) - 1 and grid[row + 1][column] == height + 1:
+            for peak_location in step_part_2(grid, row + 1, column, height + 1, limit):
+                peaks.append(peak_location)
+        if row > 0 and grid[row - 1][column] == height + 1:
+            for peak_location in step_part_2(grid, row - 1, column, height + 1, limit):
+                peaks.append(peak_location)
+        if column < len(grid[0]) - 1 and grid[row][column + 1] == height + 1:
+            for peak_location in step_part_2(grid, row, column + 1, height + 1, limit):
+                peaks.append(peak_location)
+        if column > 0 and grid[row][column - 1] == height + 1:
+            for peak_location in step_part_2(grid, row, column - 1, height + 1, limit):
+                peaks.append(peak_location)
+        return peaks
+
+
 if __name__ == "__main__":
     grid = read_input()
-    print_grid(grid)
     count = find_trails(grid)
     print(f"Total trailhead score: {count}")
